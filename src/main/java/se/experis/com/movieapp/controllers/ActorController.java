f9bfed616f019e78ccced57373b55ea33b7edce6
Aman Zadran <amanzadran9@gmail.com>
2020-10-21 14:25:54 +0200

    feat(ActorController): Endpoint for creation of Actors in db, done.

--- src/main/java/se/experis/com/movieapp/controllers/ActorController.java
+++ src/main/java/se/experis/com/movieapp/controllers/ActorController.java
@@ -3,8 +3,11 @@ package se.experis.com.movieapp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import se.experis.com.movieapp.models.Actor;
 import se.experis.com.movieapp.models.CommonResponse;
 import se.experis.com.movieapp.repositories.ActorRepository;
 
@@ -15,12 +18,25 @@ public class ActorController {
     @Autowired
     private ActorRepository actorRepository;
 
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public ResponseEntity<CommonResponse> getAllActors() {
         var commonResponse = new CommonResponse();
         commonResponse.data = actorRepository.findAll();
         commonResponse.message = "All actors";
 
         return new ResponseEntity<>(commonResponse, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<CommonResponse> addActor(@RequestBody Actor actor) {
+        var commonResponse = new CommonResponse();
+        
+        actor = actorRepository.save(actor);
+
+        commonResponse.data = actor;
+        commonResponse.message = "New actor created with the id: " +actor.id;
+
+        return new ResponseEntity<>(commonResponse,HttpStatus.CREATED);
     }
 }
 
